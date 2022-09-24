57abba83abadf1223e9ad3c64482e498bd7d949d
Eggngineer <yama_shin1999@icloud.com>
2022-09-24 19:50:38 +0900

    push: add installer(install.sh)

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s $PWD/.zshrc $HOME/.zshrc
+ln -s $PWD/.zsh $HOME/.zsh
+ln -s $PWD/.zshenv $HOME/.zshenv
+ln -s $PWD/.zprofile $HOME/.zprofile
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+
+source $HOME/.zshrc
